aed62b2047c23f80fc1cc74360fca2133c7d40b6
Emuchay <kelmuchy@gmail.com>
2025-10-10 20:52:15 +0100

    feat: add Navbar, About components and layout structure; include global styles and favicon

---
+++ app/layout.js
@@ -0,0 +1,32 @@
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  subsets: ["latin"], weight: ["400", "500", "600", "700"]
+});
+
+const ovo = Ovo({
+  subsets: ["latin"], weight: ["400"]
+}); 
+
+export const metadata = {
+  title: "Portfolio - Emuch",
+  description: "Portfolio website of Emuch",
+};
+
+/**
+ * Root layout component for the application.
+ * @param {{ children: React.ReactNode }} props - The children elements to render inside the layout.
+ * @returns {JSX.Element} The root layout structure.
+ */
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,15 @@
+'use client'
+import Navbar from "@/app/components/Navbar";
+import Header from "@/app/components/Header";
+import About from "@/app/components/About";
+import { use } from "react";
+
+export default function Home() {
+  return (
+   <>
+   <Navbar />
+   <Header />
+   <About />
+   </>
+  );
+}
