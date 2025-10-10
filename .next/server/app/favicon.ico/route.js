57acd478a4781ac7a226e2280fab75d4682befec
Emuchay <kelmuchy@gmail.com>
2025-10-10 20:58:07 +0100

    Add server-side rendering chunks and manifest for external modules
    - Created new SSR chunk files for external dependencies including React, path, and OpenTelemetry API.
    - Added source maps for the newly created SSR chunks.
    - Introduced a server reference manifest to manage node and edge references along with an encryption key.

---
+++ .next/server/app/favicon.ico/route.js
@@ -0,0 +1,6 @@
+var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
+R.c("server/chunks/c7c6d_next_0757f4aa._.js")
+R.c("server/chunks/[root-of-the-server]__2f908a86._.js")
+R.m("[project]/portfolio/Portfolio-website/.next-internal/server/app/favicon.ico/route/actions.js [app-rsc] (server actions loader, ecmascript)")
+R.m("[project]/portfolio/Portfolio-website/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/portfolio/Portfolio-website/app/favicon--route-entry.js [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/portfolio/Portfolio-website/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/portfolio/Portfolio-website/app/favicon--route-entry.js [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
